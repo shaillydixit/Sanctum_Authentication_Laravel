ea65b97656a8dfcc27c0089d1685dc205ffbb29e
shaillydixit <76932875+shaillydixit@users.noreply.github.com>
2021-07-16 00:06:17 +0530

    initial commit with table and model

---
+++ database/migrations/2021_07_15_182241_create_students_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 120);
+            $table->string('email', 60);
+            $table->string('password', 80);
+            $table->string('phone_no', 20);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
