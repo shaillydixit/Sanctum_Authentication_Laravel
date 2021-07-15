9c8ab00d5916b42d4b2a8c47791954c000fb9e5a
shaillydixit <76932875+shaillydixit@users.noreply.github.com>
2021-07-16 03:55:59 +0530

    project all methods created delete, get, post

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -4,26 +4,91 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class ProjectController extends Controller
 {
     //Create Project API
-    public function createProject()
+    public function createProject(Request $request)
     {
+        //validation
+        $request->validate([
+            'name' => 'required',
+            'description' => 'required',
+            'duration' => 'required'
+        ]);
+
+        //student id + create data
+        $student_id = auth()->user()->id;
+
+        $project = new Project();
+
+        $project->student_id = $student_id;
+        $project->name = $request->name;
+        $project->description = $request->description;
+        $project->duration = $request->duration;
+        $project->save();
+
+        //send response
+        return response()->json([
+            'status' => 1,
+            'message' => 'project has been created successfully'
+        ]);
     }
 
     //List Project API
     public function listProject()
     {
+        $student_id = auth()->user()->id;
+
+        $projects = Project::where('student_id', $student_id)->get();
+
+        return response()->json([
+            'status' => 1,
+            'message' => "Projects",
+            'data' => $projects
+        ]);
     }
 
     //Single Project API
-    public function singleProject()
+    public function singleProject($id)
     {
+        $student_id = auth()->user()->id;
+        if (Project::where(['id' => $id, 'student_id' => $student_id])->exists()) {
+            $details = Project::where(['id' => $id, 'student_id' => $student_id])->first();
+            return response()->json([
+                'status' => 1,
+                'message' => 'Project Details',
+                'data' => $details
+            ]);
+        } else {
+            return response()->json([
+                'status' => 0,
+                'message' => 'Project not found',
+            ]);
+        }
     }
 
     //Delete Project API
-    public function deleteProject()
+    public function deleteProject($id)
     {
+        $student_id = auth()->user()->id;
+        if (Project::where(['id' => $id, 'student_id' => $student_id])->exists()) {
+            $project = Project::where([
+                'id' => $id,
+                'student_id' => $student_id
+            ])->first();
+            $project->delete();
+
+            return response()->json([
+                'status' => 1,
+                'message' => 'Project has been deleted successfully'
+            ]);
+        } else {
+            return response()->json([
+                'status' => 0,
+                'message' => 'Project not found'
+            ]);
+        }
     }
 }
