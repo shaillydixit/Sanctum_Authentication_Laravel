1c5fdedc7bcbef725e3a69b7dcad9ecfbb056467
shaillydixit <76932875+shaillydixit@users.noreply.github.com>
2021-07-16 02:08:03 +0530

    login api created with access token

--- app/Http/Controllers/Api/StudentController.php
+++ app/Http/Controllers/Api/StudentController.php
@@ -36,8 +36,40 @@ class StudentController extends Controller
     }
 
     //Login API
-    public function login()
+    public function login(Request $request)
     {
+        //validation
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|confirmed'
+        ]);
+
+        //check student
+        $student = Student::where('email', '=', $request->email)->first();
+        if (isset($student->id)) {
+            if (Hash::check($request->password, $student->password)) {
+                //create a token
+                $token = $student->createToken('auth_token')->plainTextToken;
+
+                //send response
+                return response()->json([
+                    'status' => 1,
+                    'message' => ' Student loged in successfully',
+                    'access_token' => $token
+                ]);
+            } else {
+
+                return response()->json([
+                    'status' => 0,
+                    'message' => 'password did not match'
+                ], 404);
+            }
+        } else {
+            return response()->json([
+                'status' => 0,
+                'message' => 'student not found'
+            ], 404);
+        }
     }
 
     //Profile API
